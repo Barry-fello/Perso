49580b9c3f41cc51439b88d831c45eb9de9abea6
Barry <ibrahimafello2017@gmail.com>
2022-07-10 17:16:28 +0200

    Ajout des types des pokemonts et des couleurs via les pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BorderCardDirective } from './border-card.directive';
+import { PokemonColorPipe } from './pokemon-color.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BorderCardDirective
+    BorderCardDirective,
+    PokemonColorPipe
   ],
   imports: [
     BrowserModule,
